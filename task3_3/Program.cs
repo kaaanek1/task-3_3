aa7c3ad0724b4b9768c7b35aaaa6bbd1f0a1059c
Taya <151873595+kaaanek1@users.noreply.github.com>
2024-02-27 19:35:22 +0300

    Update Program.cs

--- task3_3/Program.cs
+++ task3_3/Program.cs
@@ -1,40 +1,41 @@
-﻿class Program
+namespace task3_3
 {
-    static void Main()
+    class Program
     {
-        IParent[] array = new IParent[3];
-
-        Console.Write("Будешь заполнять массивы сам?(true/false)");
-        bool fill = bool.Parse(Console.ReadLine());
-        Console.WriteLine();
-
-        Console.Write("Введите длину одномерного массива: ");
-        int l1 = int.Parse(Console.ReadLine());
-        LVL1 arr1 = new(l1, fill);
-        array[0] = arr1;
-        Console.WriteLine();
-
-        Console.Write("Введите a двумерного массива:");
-        int a = int.Parse(Console.ReadLine());
-        Console.Write("Введите b для двумерного массива:");
-        int b = int.Parse(Console.ReadLine());
-        LVL2 arr2 = new(b, a, fill);
-        array[1] = arr2;
-        Console.WriteLine();
-
-        Console.WriteLine("Введите длину ступенчатого массива:");
-        int l2 = int.Parse(Console.ReadLine());
-        LVL3 arr3 = new(l2, fill);
-        array[2] = arr3;
-
-        
-        for (int i = 0; i < 3; i++)
+        static void Main()
         {
-            array[i].Print();
-            Console.WriteLine("Среднее арифметическое:");
-            array[i].Average();
+            IParent[] array = new IParent[3];
+    
+            Console.Write("Будешь заполнять массивы сам?(true/false)");
+            bool fill = bool.Parse(Console.ReadLine());
+            Console.WriteLine();
+    
+            Console.Write("Введите длину одномерного массива: ");
+            int l1 = int.Parse(Console.ReadLine());
+            LVL1 arr1 = new(l1, fill);
+            array[0] = arr1;
+            Console.WriteLine();
+    
+            Console.Write("Введите a двумерного массива:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b для двумерного массива:");
+            int b = int.Parse(Console.ReadLine());
+            LVL2 arr2 = new(b, a, fill);
+            array[1] = arr2;
+            Console.WriteLine();
+    
+            Console.WriteLine("Введите длину ступенчатого массива:");
+            int l2 = int.Parse(Console.ReadLine());
+            LVL3 arr3 = new(l2, fill);
+            array[2] = arr3;
+    
+            
+            for (int i = 0; i < 3; i++)
+            {
+                array[i].Print();
+                Console.WriteLine("Среднее арифметическое:");
+                array[i].Average();
+            }
         }
     }
 }
-
-
